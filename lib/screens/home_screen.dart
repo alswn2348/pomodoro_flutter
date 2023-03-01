ea3c48dc53eecbada4ae7ba1b6f6b905d1692be1
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-01 11:02:37 +0900

    feat : add pause button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
+  bool isRunning = false;
   late Timer timer;
 
   void onTick(Timer timer) {
@@ -24,6 +25,16 @@ class _HomeScreenState extends State<HomeScreen> {
       const Duration(seconds: 1),
       onTick,
     );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onpausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -51,8 +62,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: IconButton(
                     iconSize: 120,
                     color: Theme.of(context).cardColor,
-                    onPressed: onStartPressed,
-                    icon: const Icon(Icons.play_circle_outline))),
+                    onPressed: isRunning ? onpausePressed : onStartPressed,
+                    icon: Icon(isRunning
+                        ? Icons.pause_circle_filled_outlined
+                        : Icons.play_circle_outline))),
           ),
           Flexible(
             flex: 1,
